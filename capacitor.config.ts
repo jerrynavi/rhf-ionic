8b9bc8d7939cc3545df06590c97b280c4c000b23
Jerry Ejonavi <jerryasih@gmail.com>
2021-06-17 13:55:32 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rhf-ionic',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
